16220738bdd046e163652e8d4d444930f0934ae9
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-02-02 14:08:42 +0100

    [add] usePaginatedList hook

---
+++ packages/react-hooks/src/usePaginatedList.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function <PageType, ReturnType>(
+    pages: PageType[] = [],
+    getItems: (page: PageType) => ReturnType[],
+): ReturnType[] {
+    const [items, setItems] = useState<ReturnType[]>([]);
+    const [pagesLength, setPagesLength] = useState(0);
+
+    useEffect(() => {
+        setItems((previousItems) => {
+            let newItems: ReturnType[] = [];
+            const newPages = pages.slice(pagesLength);
+            newPages.forEach((page) => (newItems = newItems.concat(getItems(page))));
+            setPagesLength(pages.length);
+            return [...previousItems, ...newItems];
+        });
+    }, [pages.length, getItems]);
+
+    return items;
+}
