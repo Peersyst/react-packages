5ab1367d302d3bc38ea6028dbf4defdf58d9c97a
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-02-07 16:41:33 +0100

    [fix] Translate fn type

--- packages/react-components/src/TextInput/utils/getValidators.ts
+++ packages/react-components/src/TextInput/utils/getValidators.ts
@@ -1,13 +1,14 @@
 import { FunctionalValidator } from "../TextInput.types";
 import { BaseValidator } from "./Validators/BaseValidator";
 import { parseValidators } from "./ValidatorParser";
+import { TranslateFn } from "./Validators.types";
 
 export type CustomValidators = (BaseValidator | FunctionalValidator)[] | undefined;
 
 export function getValidators(
     validators = "",
     customValidators: CustomValidators = [],
-    translate: (w: string) => string,
+    translate: TranslateFn,
 ): BaseValidator[] {
     return parseValidators(validators, translate).concat(customValidators);
 }
