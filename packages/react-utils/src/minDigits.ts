89a408e5020573fbe931a293894248165a97c0f7
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-03-11 10:24:27 +0100

    [react-utils] added minDigits options

--- packages/react-utils/src/minDigits.ts
+++ packages/react-utils/src/minDigits.ts
@@ -1,4 +1,14 @@
-export default function (n: number | string, d: number): string {
+export interface MinDigitsOptions {
+    fill?: "left" | "right";
+}
+
+export default function (
+    n: number | string,
+    d: number,
+    { fill = "left" }: MinDigitsOptions = {},
+): string {
     const num = n.toString();
-    return [...Array(d - num.length)].reduce((prev = "") => prev + "0", "") + num;
+    const dif = d - num.length;
+    const zeros = dif > 0 ? [...Array(dif)].reduce((prev = "") => prev + "0", "") : "";
+    return (fill === "left" ? zeros : "") + num + (fill === "right" ? zeros : "");
 }
