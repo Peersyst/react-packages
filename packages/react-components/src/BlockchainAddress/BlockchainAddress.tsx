584fdcb740e04dba5e3c52b3e6f6a63132fdbbcd
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-03-25 12:50:29 +0100

    [react-utils] formatAddress patch

--- packages/react-components/src/BlockchainAddress/BlockchainAddress.tsx
+++ packages/react-components/src/BlockchainAddress/BlockchainAddress.tsx
@@ -3,7 +3,29 @@ import { Row } from "../Row";
 import { Typography } from "../Typography";
 import { CopyButton } from "../CopyButton";
 import { useTheme } from "../Theme";
-import { cx, formatAddress } from "@peersyst/react-utils";
+import { cx } from "@peersyst/react-utils";
+
+// react-utils
+export type BlockchainAddressEllipsis = "middle" | "end";
+function formatAddress(
+    address: string,
+    ellipsis: BlockchainAddressEllipsis = "middle",
+    length?: number,
+): string {
+    const addressLength = address.length;
+    if (!length) return address;
+    else if (ellipsis === "middle") {
+        if (addressLength - 2 <= length * 2) return address;
+        return (
+            address.substring(0, 2 + length) +
+            "..." +
+            address.substring(addressLength - length, addressLength)
+        );
+    } else {
+        if (addressLength - 2 <= length) return address;
+        return address.substring(0, 2 + length) + "...";
+    }
+}
 
 const BlockchainAddress = ({
     address,
