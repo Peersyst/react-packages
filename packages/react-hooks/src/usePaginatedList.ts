8a83c291ae36072575154392d16c40be7f539670
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-03-28 19:07:01 +0200

    [react-hooks] Fixed usePaginatedList updates

--- packages/react-hooks/src/usePaginatedList.ts
+++ packages/react-hooks/src/usePaginatedList.ts
@@ -1,21 +1,12 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 
 export default function <PageType, ReturnType>(
     pages: PageType[] = [],
     getItems: (page: PageType) => ReturnType[],
 ): ReturnType[] {
-    const [items, setItems] = useState<ReturnType[]>([]);
-    const [pagesLength, setPagesLength] = useState(0);
-
-    useEffect(() => {
-        setItems((previousItems) => {
-            let newItems: ReturnType[] = [];
-            const newPages = pages.slice(pagesLength);
-            newPages.forEach((page) => (newItems = newItems.concat(getItems(page))));
-            setPagesLength(pages.length);
-            return [...previousItems, ...newItems];
-        });
-    }, [pages.length]);
-
-    return items;
+    return useMemo(() => {
+        let items: ReturnType[] = [];
+        pages.forEach((page) => (items = items.concat(getItems(page))));
+        return items;
+    }, [pages]);
 }
