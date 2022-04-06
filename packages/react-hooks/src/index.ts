74cc8a0aed7a56ab0e918b9035b51211c0340005
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-04-06 17:05:36 +0200

    [react-hooks] added useMediaQuery

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -1,6 +1,7 @@
 export { default as useAppendChild } from "./useAppendChild";
 export { default as useControlled } from "./useControlled";
 export { default as useForkRef } from "./useForkRef";
+export { default as useMediaQuery } from "./useMediaQuery";
 export { default as usePaginatedList } from "./usePaginatedList";
 export { default as usePreventBodyScroll } from "./usePreventBodyScroll";
 
