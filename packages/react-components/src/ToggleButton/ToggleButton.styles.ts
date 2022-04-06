6f62860c3482e6178220f4744493dee1204e2870
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-04-06 10:59:57 +0200

    [react-components] ToggleButton color transition

--- packages/react-components/src/ToggleButton/ToggleButton.styles.ts
+++ packages/react-components/src/ToggleButton/ToggleButton.styles.ts
@@ -4,6 +4,7 @@ import { getLuminance } from "@peersyst/react-utils";
 
 export const ToggleButtonRoot = styled(Button)(
     ({ theme }) => css`
+        transition: color 100ms, background-color 100ms;
         &.Selected {
             background-color: ${theme.palette.primary};
             color: ${getLuminance(theme.palette.primary) > 0.7 ? "black" : "white"};
