c9aa7c2f5f3b9d02e4123d3c96f6923db8669d01
Jordi Parra Crespo <jordiparra99@gmail.com>
2022-06-27 09:07:39 +0200

    fix: improve types

--- packages/react-components/src/BlockchainAddress/BlockchainAddress.types.ts
+++ packages/react-components/src/BlockchainAddress/BlockchainAddress.types.ts
@@ -1,19 +1,17 @@
 import { OverridableStringUnion } from "@peersyst/react-types";
-import { TypographyProps } from "../Typography";
 import { BlockchainLinksTypes, BlockchainLinksTypesOverrides } from "../styles";
 import { HashProps } from "src/Hash";
 
-export type BlockchainAddressProps = Omit<TypographyProps, "singleLine" | "children"> &
-    Omit<HashProps, "url" | "hash"> & {
-        /**
-         * Blockchain address
-         */
-        address: string;
-        /**
-         * Blockchain address type
-         */
-        type: OverridableStringUnion<BlockchainLinksTypes, BlockchainLinksTypesOverrides>;
-    };
+export type BlockchainAddressProps = Omit<HashProps, "url" | "hash"> & {
+    /**
+     * Blockchain address
+     */
+    address: string;
+    /**
+     * Blockchain address type
+     */
+    type: OverridableStringUnion<BlockchainLinksTypes, BlockchainLinksTypesOverrides>;
+};
 
 export interface BlockchainAddressRootProps {
     autoLength: boolean;
