7d62689795a54804a6a3be041013351641ffc478
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-08-01 12:18:48 +0200

    Fix icons strokes and fills

--- packages/genesys/packages/react-components/src/SvgIcon/SvgIcon.styles.ts
+++ packages/genesys/packages/react-components/src/SvgIcon/SvgIcon.styles.ts
@@ -4,6 +4,9 @@ export const ExtendedSvg = styled.svg`
     &:not(.Filled) {
         path,
         rect {
+            fill: none;
+            stroke: none;
+
             &[fill]:not([fill="none"]) {
                 fill: currentColor;
             }
