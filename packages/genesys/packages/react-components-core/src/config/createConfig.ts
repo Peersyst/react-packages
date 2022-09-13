a74a263098b0b1f7fd256eea887f096e8ab3dd8a
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-09-13 12:34:58 +0200

    Fix createConfigCore rest defaults

--- packages/genesys/packages/react-components-core/src/config/createConfig.ts
+++ packages/genesys/packages/react-components-core/src/config/createConfig.ts
@@ -1,17 +1,24 @@
-import { Config, CoreConfig } from "./config.types";
+import { Config } from "./config.types";
 import { deepmerge } from "@peersyst/react-utils";
 import createThemes from "../theme/createThemes";
 import { CreateConfig } from "./createConfig.types";
 
 export default function createConfigCore(
-    defaultConfig: Omit<CoreConfig, "projectName">,
+    {
+        themes: defaultThemes,
+        translate: defaultTranslate,
+        validators: defaultValidators,
+        components: defaultComponents,
+        ...restDefaults
+    }: Omit<Config, "projectName">,
     { themes, translate, validators, components, ...extraConfig }: CreateConfig,
 ): Config {
     return {
-        themes: createThemes(defaultConfig.themes, themes),
-        translate: translate || defaultConfig.translate,
-        validators: { ...defaultConfig.validators, ...validators },
-        components: deepmerge(defaultConfig.components, components || {}),
+        themes: createThemes(defaultThemes, themes),
+        translate: translate || defaultTranslate,
+        validators: { defaultValidators, ...validators },
+        components: deepmerge(defaultComponents, components || {}),
+        ...restDefaults,
         ...extraConfig,
     };
 }
