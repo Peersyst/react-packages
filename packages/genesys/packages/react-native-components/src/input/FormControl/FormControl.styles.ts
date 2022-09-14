390f30e5a8d505f594c10db4a52e13638e5e7484
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-09-14 09:19:07 +0200

    Fix rn FormControl width

--- packages/genesys/packages/react-native-components/src/input/FormControl/FormControl.styles.ts
+++ packages/genesys/packages/react-native-components/src/input/FormControl/FormControl.styles.ts
@@ -2,5 +2,5 @@ import styled from "@peersyst/react-native-styled";
 import { Col } from "../../layout/Col";
 
 export const FormControlRoot = styled(Col)(() => ({
-    alignSelf: "stretch",
+    width: "100%",
 }));
