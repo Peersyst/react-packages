d94c0dd040ee05e5ac6b6c57f58b403a219ec33b
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-09-14 13:15:45 +0200

    Add select context re export

--- packages/genesys/packages/react-components/src/Select/index.tsx
+++ packages/genesys/packages/react-components/src/Select/index.tsx
@@ -3,3 +3,4 @@ export * from "./Select.styles";
 export * from "./Select.types";
 export * from "./SelectMenu";
 export * from "./SelectItem";
+export { SelectContext, useSelected } from "@peersyst/react-components-core";
--- packages/genesys/packages/react-native-components/src/input/Select/index.tsx
+++ packages/genesys/packages/react-native-components/src/input/Select/index.tsx
@@ -2,3 +2,4 @@ export { default as Select } from "./Select";
 export * from "./Select.types";
 export * from "./SelectMenu";
 export * from "./SelectItem";
+export { SelectContext, useSelected } from "@peersyst/react-components-core";
