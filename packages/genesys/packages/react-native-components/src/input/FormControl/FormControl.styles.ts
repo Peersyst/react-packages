a33b184451ca89986f38cb910f4164d38b8ac361
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-09-19 08:32:33 +0200

    FormControl auto width

--- packages/genesys/packages/react-native-components/src/input/FormControl/FormControl.styles.ts
+++ packages/genesys/packages/react-native-components/src/input/FormControl/FormControl.styles.ts
@@ -2,5 +2,5 @@ import styled from "@peersyst/react-native-styled";
 import { Col } from "../../layout/Col";
 
 export const FormControlRoot = styled(Col)(() => ({
-    width: "100%",
+    width: "auto",
 }));
