dc6714ee1d5ef30b2242d1a91d900025fc4d0ede
JordiParraCrespo <jordiparra99@gmail.com>
2022-09-22 12:47:50 +0200

    Add global styles to Divider

--- packages/genesys/packages/react-native-components/src/display/Divider/Divider.tsx
+++ packages/genesys/packages/react-native-components/src/display/Divider/Divider.tsx
@@ -1,16 +1,18 @@
 import { DividerRoot, DividerWithChildren } from "./Divider.styles";
 import { DividerProps } from "./Divider.types";
 import { useMergeDefaultProps } from "@peersyst/react-components-core";
+import { useGlobalStyles } from "../../config";
 
 export default function Divider(props: DividerProps): JSX.Element {
     const {
         size = 1,
         width: widthProp = "full-width",
-        style,
+        style: styleProp,
         color,
         children,
     } = useMergeDefaultProps("Divider", props);
-
+    const defaultStyle = useGlobalStyles("Divider");
+    const style = { ...defaultStyle, ...styleProp };
     const {
         backgroundColor,
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -25,17 +27,13 @@ export default function Divider(props: DividerProps): JSX.Element {
     } = style || {};
     const divStyle = { backgroundColor, minWidth, minHeight, maxHeight, maxWidth };
 
-    return (
-        <>
-            {children ? (
-                <DividerWithChildren width={widthProp} style={restStyle}>
-                    <DividerRoot height={size} color={color} width="full-width" style={divStyle} />
-                    {children}
-                    <DividerRoot height={size} color={color} width="full-width" style={divStyle} />
-                </DividerWithChildren>
-            ) : (
-                <DividerRoot height={size} color={color} width={widthProp} style={style} />
-            )}
-        </>
+    return children ? (
+        <DividerWithChildren width={widthProp} style={restStyle}>
+            <DividerRoot height={size} color={color} width="full-width" style={divStyle} />
+            {children}
+            <DividerRoot height={size} color={color} width="full-width" style={divStyle} />
+        </DividerWithChildren>
+    ) : (
+        <DividerRoot height={size} color={color} width={widthProp} style={style} />
     );
 }
