55826458d6dd0432d81701e2497ff37350861993
JordiParraCrespo <jordiparra99@gmail.com>
2022-10-07 12:11:57 +0200

    remove unneccessary styles

--- packages/genesys/packages/react-native-components/src/input/Switch/Switch.styles.ts
+++ packages/genesys/packages/react-native-components/src/input/Switch/Switch.styles.ts
@@ -3,11 +3,11 @@ import { Animated, View } from "react-native";
 import { SwitchThumbProps, SwitchTrackStyle } from "./Switch.types";
 
 export const SwitchThumb = styled(Animated.View)<SwitchThumbProps>(
-    ({ height, width, padding, backgroundColor, theme: { palette: p } }) => ({
+    ({ height, width, padding, backgroundColor }) => ({
         height: height ?? 24,
         width: width ?? 38,
         padding: padding ?? 5,
-        backgroundColor: backgroundColor ?? p.primary,
+        backgroundColor: backgroundColor,
         borderRadius: 2000,
     }),
 );
@@ -27,11 +27,9 @@ export const SwitchElementWrapper = styled(Animated.View)(() => ({
     flexDirection: "row",
 }));
 
-export const SwitchTrack = styled(Animated.View)<SwitchTrackStyle>(
-    ({ backgroundColor, theme: { palette: p } }) => ({
-        height: "100%",
-        width: "50%",
-        borderRadius: 2000,
-        backgroundColor: backgroundColor ?? p.background,
-    }),
-);
+export const SwitchTrack = styled(Animated.View)<SwitchTrackStyle>(({ backgroundColor }) => ({
+    height: "100%",
+    width: "50%",
+    borderRadius: 2000,
+    backgroundColor: backgroundColor,
+}));
