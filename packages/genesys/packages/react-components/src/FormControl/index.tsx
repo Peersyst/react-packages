6ca6a347c168993168ae4bfe95e1775735f3f11e
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-10-14 07:40:00 +0200

    Reexport CoreFormControlledComponentProps

--- packages/genesys/packages/react-components/src/FormControl/index.tsx
+++ packages/genesys/packages/react-components/src/FormControl/index.tsx
@@ -1,2 +1,3 @@
 export { default as FormControl } from "./FormControl";
 export * from "./FormControl.types";
+export type { CoreFormControlledComponentProps } from "@peersyst/react-components-core";
--- packages/genesys/packages/react-native-components/src/input/FormControl/index.tsx
+++ packages/genesys/packages/react-native-components/src/input/FormControl/index.tsx
@@ -1,3 +1,4 @@
 export { default as FormControl } from "./FormControl";
 export * from "./FormControl.styles";
 export * from "./FormControl.types";
+export type { CoreFormControlledComponentProps } from "@peersyst/react-components-core";
