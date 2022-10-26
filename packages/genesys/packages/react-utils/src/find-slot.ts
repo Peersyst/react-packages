71bca94da0fa4ba5feef2a8f8556a62bd7de9206
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-10-26 14:35:50 +0200

    Slot utilities

--- packages/genesys/packages/react-utils/src/find-slot.ts
+++ packages/genesys/packages/react-utils/src/find-slot.ts
@@ -1,5 +1,10 @@
 import { Children, JSXElementConstructor, ReactElement, ReactNode } from "react";
 
+/**
+ * Deprecated
+ * @param children
+ * @param type
+ */
 export default function findSlot<TElement extends JSXElementConstructor<any>>(
     children: ReactNode,
     type: any,
