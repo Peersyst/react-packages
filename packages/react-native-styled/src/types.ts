340f9b662489df38df0782e2c3b8e9e111b04df8
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-02-16 11:18:33 +0100

    Restore SX type

--- packages/react-native-styled/src/types.ts
+++ packages/react-native-styled/src/types.ts
@@ -2,6 +2,8 @@ import { Theme } from "@peersyst/react-native-styled";
 import { ScaledSize } from "react-native";
 import { EdgeInsets } from "react-native-safe-area-context";
 
+export type SX<TStyle> = (theme: Theme) => TStyle;
+
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type StyledFunction<P extends { style?: P["style"] }, E = {}> = (
     p: { theme: Theme } & { dimensions: ScaledSize; safeAreaInsets: EdgeInsets } & E,
