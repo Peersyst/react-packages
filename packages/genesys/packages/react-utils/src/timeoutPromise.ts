5b361d66a535b9fb726e078965a42715e8dc5618
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-02-22 16:33:03 +0100

    Fix timeout promise

--- packages/genesys/packages/react-utils/src/timeoutPromise.ts
+++ packages/genesys/packages/react-utils/src/timeoutPromise.ts
@@ -1,14 +1,18 @@
 export class TimeoutPromiseError extends Error {
     constructor() {
-        super("Promise timed out");
+        super("Promise timeout");
     }
 }
 
 export default async function timeoutPromise<T>(promise: Promise<T>, ms: number): Promise<T> {
-    const timeout = setTimeout(() => {
-        Promise.reject(new TimeoutPromiseError());
-    }, ms);
-    const res = await promise;
+    let timeout;
+    const rejectedPromise = new Promise<T>((_, reject) => {
+        timeout = setTimeout(() => {
+            reject(new TimeoutPromiseError());
+        }, ms);
+    });
+
+    const res = await Promise.race([promise, rejectedPromise]);
     clearTimeout(timeout);
     return res;
 }
