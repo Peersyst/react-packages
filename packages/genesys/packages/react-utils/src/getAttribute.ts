e421380838387e1f5b154c7c283a96887c413bd5
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-02-23 12:38:07 +0100

    Fix getAttribute excessively deep iterations

--- packages/genesys/packages/react-utils/src/getAttribute.ts
+++ packages/genesys/packages/react-utils/src/getAttribute.ts
@@ -1,8 +1,10 @@
 import { DeepPick, NestedKeys } from "@peersyst/react-types";
 
+// @ts-ignore Not excessively deep (max 10 levels)
 export default function <T extends object, K extends NestedKeys<T>>(
     obj: T,
     key: K,
+    // @ts-ignore Not excessively deep (max 10 levels)
 ): DeepPick<T, K> {
     const [firstKey, ...restKeys] = key.split(".");
 
