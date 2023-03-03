4ec476ad215dda9e2b96f0ed82c837ae973fd8a4
JordiParraCrespo <jordiparra99@gmail.com>
2023-03-03 12:38:11 +0100

    remove component type

--- packages/genesys/packages/react-components/src/utils/hoc/withColor/withColor.tsx
+++ packages/genesys/packages/react-components/src/utils/hoc/withColor/withColor.tsx
@@ -1,10 +1,10 @@
 import { useColor } from "@peersyst/react-components-core";
-import { ComponentType } from "react";
+import { JSXElementConstructor } from "react";
 import { WithColor, WithColorProps } from "./withColor.types";
 
 export default function withColor<TProps extends WithColorProps>(
-    WrappedComponent: ComponentType<TProps>,
-): ComponentType<WithColor<TProps>> {
+    WrappedComponent: JSXElementConstructor<TProps>,
+): JSXElementConstructor<WithColor<TProps>> {
     return ({ color: colorProp, ...componentProps }: WithColor<TProps>) => {
         const color = useColor(colorProp);
         return <WrappedComponent {...(componentProps as TProps)} color={color} />;
