5106b7a6ca8f27afa5c9d2c92e0e688f51c1a443
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-03-06 08:36:02 +0100

    Fix Table.extensions

--- packages/genesys/packages/react-components/src/Table/index.tsx
+++ packages/genesys/packages/react-components/src/Table/index.tsx
@@ -1,5 +1,5 @@
 export { default as Table } from "./Table";
 export * from "./Table.types";
 export type { ColumnDef } from "@tanstack/react-table";
-import "./Table.extensions.d";
 export * from "./PaginatedTable";
+import "./Table.extensions";
