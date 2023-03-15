6eb359e4ff021da843b742541637ad369d1319e7
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-03-15 15:25:48 +0100

    Fix ts

--- packages/genesys/packages/react-native-components/src/input/TextInput/TextField.tsx
+++ packages/genesys/packages/react-native-components/src/input/TextInput/TextField.tsx
@@ -1,14 +1,11 @@
 import { TextFieldProps } from "./TextInput.types";
 import TextInput from "./TextInput";
 import { useMergeDefaultProps } from "@peersyst/react-components-core";
-import { TextInputProps } from "react-native";
 
-function TextField<P extends TextInputProps = TextInputProps>(
-    props: TextFieldProps<P>,
-): JSX.Element {
+function TextField(props: TextFieldProps): JSX.Element {
     const mergedProps = useMergeDefaultProps("TextField", props);
 
-    return <TextInput<P> {...mergedProps} />;
+    return <TextInput {...mergedProps} />;
 }
 
 export default TextField;
