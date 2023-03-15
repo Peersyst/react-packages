7f58647d9effd8a581d0c0463c33314122830338
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-03-15 16:33:30 +0100

    Reexport useNumericInput

--- packages/genesys/packages/react-components/src/TextInput/index.tsx
+++ packages/genesys/packages/react-components/src/TextInput/index.tsx
@@ -1,6 +1,4 @@
 export { default as TextInput } from "./TextInput";
-export { default as TextField } from "./TextField";
-export { default as TextArea } from "./TextArea";
 export * from "./TextInput.types";
-export { TextInputRoot } from "./TextInput.styles";
+export * from "./TextInput.styles";
 export { useNumericInput } from "@peersyst/react-components-core";
