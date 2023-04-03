b5663f7116da4b0f98f2db5907a7ce629a41c2d5
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-04-03 09:08:44 +0200

    Add playground

---
+++ packages/genesys/packages/react-native-components/playground/playgrounds/index.ts
@@ -0,0 +1,7 @@
+import Welcome from "./Welcome.playground";
+import Button from "./Button.playground";
+
+export default {
+  Welcome,
+  Button,
+};
--- packages/genesys/packages/react-native-components/src/input/Button/Button.styles.ts
+++ packages/genesys/packages/react-native-components/src/input/Button/Button.styles.ts
@@ -3,7 +3,7 @@ import styled from "@peersyst/react-native-styled";
 import { GradientView } from "../../layout/GradientView";
 
 export const ButtonRoot = styled(GradientView)<ButtonRootProps>(({ theme, fullWidth }) => ({
-    alignSelf: fullWidth ? "stretch" : "flex-start",
+    alignSelf: fullWidth ? "stretch" : undefined,
     justifyContent: "center",
     borderRadius: theme.borderRadius,
 }));
