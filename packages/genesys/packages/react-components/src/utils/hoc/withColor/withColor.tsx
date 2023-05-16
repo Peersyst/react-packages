f07537ea9a5a860fbf8c179835e57a37e84cc526
JordiParraCrespo <jordiparra99@gmail.com>
2023-05-16 12:56:09 +0200

    fix: remove the default color as an interface

--- packages/genesys/packages/react-components/src/utils/hoc/withColor/withColor.tsx
+++ packages/genesys/packages/react-components/src/utils/hoc/withColor/withColor.tsx
@@ -1,13 +1,12 @@
-import { useColor } from "@peersyst/react-components-core";
+import { ThemeColor, useColor } from "@peersyst/react-components-core";
 import { JSXElementConstructor } from "react";
-import { ThemeColorProps, WithColor, WithColorProps } from "./withColor.types";
+import { WithColor, WithColorProps } from "./withColor.types";
 
 export default function withColor<TProps extends WithColorProps>(
     WrappedComponent: JSXElementConstructor<TProps>,
-    colorProps: ThemeColorProps = {},
+    defaultColor?: ThemeColor,
 ): JSXElementConstructor<WithColor<TProps>> {
     return ({ color: colorProp, ...componentProps }: WithColor<TProps>) => {
-        const { color: defaultColor } = colorProps;
         const color = useColor(colorProp || defaultColor);
         return <WrappedComponent {...(componentProps as TProps)} color={color} />;
     };
