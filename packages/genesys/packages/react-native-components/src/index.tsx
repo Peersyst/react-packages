d28f63205b0d065fdf1f186fca4d9541e4da9ef3
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-06-01 15:53:55 +0200

    fix(FlashList): Fixed exporting

--- packages/genesys/packages/react-native-components/src/index.tsx
+++ packages/genesys/packages/react-native-components/src/index.tsx
@@ -13,6 +13,7 @@ export * from "./display/ScrollView";
 export * from "./display/SectionList";
 export * from "./display/SvgIcon";
 export * from "./display/Typography";
+export * from "./display/FlashList";
 export * from "./feedback/Alert";
 export * from "./feedback/Backdrop";
 export * from "./feedback/CenteredLoader";
