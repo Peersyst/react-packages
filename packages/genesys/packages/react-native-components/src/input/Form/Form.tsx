4099798a0edd5c55765aad8f20abdf952a5718a3
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-06-26 10:21:43 +0200

    fix(app): Keyboard dismissal on submit

--- packages/genesys/packages/react-native-components/src/input/Form/Form.tsx
+++ packages/genesys/packages/react-native-components/src/input/Form/Form.tsx
@@ -1,12 +1,17 @@
 import { FormProps } from "./Form.types";
 import { Form as CoreForm, useMergeDefaultProps } from "@peersyst/react-components-core";
-import { View } from "react-native";
+import { Keyboard, View } from "react-native";
 
 const Form = (props: FormProps): JSX.Element => {
-    const { style, children, ...coreProps } = useMergeDefaultProps("Form", props);
+    const { style, children, onSubmit, ...coreProps } = useMergeDefaultProps("Form", props);
+
+    const handleSubmit = (data: any, action?: string) => {
+        Keyboard.dismiss();
+        onSubmit(data, action);
+    };
 
     return (
-        <CoreForm {...coreProps}>
+        <CoreForm onSubmit={handleSubmit} {...coreProps}>
             <View style={style}>{children}</View>
         </CoreForm>
     );
