a9cbf49ff19015e0b3b750b5bbb2cdf9b14108ea
JordiParraCrespo <jordiparra99@gmail.com>
2023-12-12 15:23:15 +0100

    style(rnc): improve comment in the hook

--- packages/genesys/packages/react-native-components/src/feedback/Backdrop/hooks/useBackdropDeviceHeight.ts
+++ packages/genesys/packages/react-native-components/src/feedback/Backdrop/hooks/useBackdropDeviceHeight.ts
@@ -2,7 +2,9 @@ import { useWindowDimensions, Platform } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 /*
- * This hook is a workaround for the backdrop component on android devices
+ * This hook is a workaround for the backdrop component on android devices.
+ * The backdrop component is not able to cover the whole screen on android devices
+ * @see https://github.com/react-native-modal/react-native-modal#the-backdrop-is-not-completely-filledcovered-on-some-android-devices-galaxy-for-one
  */
 export default function useBackdropDeviceHeight(): undefined | number {
     const { height } = useWindowDimensions();
