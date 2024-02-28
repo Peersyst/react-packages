c9de46e92b65fb0bdc93423771ad7025a9a15ce3
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-02-28 11:39:03 +0100

    [RNC][RNS][RNU] feat: updating expo version

--- packages/genesys/packages/react-native-components/playground/app/App.tsx
+++ packages/genesys/packages/react-native-components/playground/app/App.tsx
@@ -1,21 +1,22 @@
-import { ConfigProvider, StatusBar, ToastProvider } from "@peersyst/react-native-components";
-import Navigator from "./Navigator";
-import stylesheets from "../stylesheets";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { StylesheetProvider } from "@peersyst/react-native-styled";
-import config from "./config";
+// import { ConfigProvider, StatusBar, ToastProvider } from "@peersyst/react-native-components";
+// // import Navigator from "./Navigator";
+// import stylesheets from "../stylesheets";
+// // import { SafeAreaProvider } from "react-native-safe-area-context";
+// import { StylesheetProvider } from "@peersyst/react-native-styled";
+// import config from "./config";
 
 export default function App() {
     return (
-        <SafeAreaProvider>
-            <ConfigProvider config={config}>
-                <StylesheetProvider stylesheets={stylesheets}>
-                    <ToastProvider>
-                        <Navigator />
-                        <StatusBar />
-                    </ToastProvider>
-                </StylesheetProvider>
-            </ConfigProvider>
-        </SafeAreaProvider>
+        // <SafeAreaProvider>
+        // <ConfigProvider config={config}>
+        //     <StylesheetProvider stylesheets={stylesheets}>
+        //         <ToastProvider>
+        //             {/* <Navigator /> */}
+        //             <StatusBar />
+        //         </ToastProvider>
+        //     </StylesheetProvider>
+        // </ConfigProvider>
+        // </SafeAreaProvider>
+        <></>
     );
 }
