26d17fb99740ffc80de9f162056951305f560dda
AgustinMJ <agustinmillanjimenez+github@gmail.com>
2024-06-10 12:05:48 +0200

    [RC] feat: add `ModalProps` to `DialogProps`

--- packages/genesys/packages/react-components/src/Dialog/Dialog.types.ts
+++ packages/genesys/packages/react-components/src/Dialog/Dialog.types.ts
@@ -3,10 +3,13 @@ import { DIALOG_SIZES } from "./Dialog.constants";
 import { Demand } from "@peersyst/react-types";
 import { ButtonProps } from "../Button";
 import { ComponentsConfig } from "../config";
+import { ModalProps } from "../Modal";
 
 export type DialogSize = typeof DIALOG_SIZES[number];
 
-export interface DialogProps extends CoreDialogProps<ComponentsConfig["Dialog"]> {
+export interface DialogProps
+    extends CoreDialogProps<ComponentsConfig["Dialog"]>,
+        Omit<ModalProps, "onExited" | "onClose"> {
     size?: DialogSize | string;
 }
 
