df62bfb1e4eb97060b2a893c78dadc310fffdcd8
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-12-11 17:55:46 +0100

    [rc] refactor: add missing roots to `table` and `paginated-table`

--- packages/genesys/packages/react-components/src/Table/PaginatedTable/index.tsx
+++ packages/genesys/packages/react-components/src/Table/PaginatedTable/index.tsx
@@ -4,3 +4,5 @@ export * from "./PaginatedTableFooter";
 export * from "./TablePagination";
 export * from "./TableCount";
 export * from "./hooks";
+export * from "./PaginatedTable.styles";
+export * from "./PaginatedTable.types";
--- packages/genesys/packages/react-components/src/Table/index.tsx
+++ packages/genesys/packages/react-components/src/Table/index.tsx
@@ -1,5 +1,6 @@
 export { default as Table } from "./Table";
 export * from "./Table.types";
+export * from "./Table.styles";
 export type { ColumnDef, PaginationState, SortingState } from "@tanstack/react-table";
 export * from "./PaginatedTable";
 import "./Table.extensions";
