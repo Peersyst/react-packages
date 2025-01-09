868473dbe89e713e9eac36884c3d47353ad52076
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2025-01-09 17:20:20 +0100

    [rcn] fix: playground

--- packages/genesys/packages/react-native-components/playground/metro.config.js
+++ packages/genesys/packages/react-native-components/playground/metro.config.js
@@ -9,24 +9,23 @@ const projectRoot = __dirname;
 // This can be replaced with `find-yarn-workspace-root`
 const workspaceRoot = path.resolve(projectRoot, "../../../../..");
 const packageRoot = path.resolve(projectRoot, "..");
-const packagesRoot = path.resolve(projectRoot, "../..");
 
 // 1. Watch all files within the monorepo
 config.watchFolders = [workspaceRoot];
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
+    ...config.resolver.nodeModulesPaths,
     path.resolve(projectRoot, "node_modules"),
-    path.resolve(workspaceRoot, "node_modules"),
     path.resolve(packageRoot, "node_modules"),
+    path.resolve(workspaceRoot, "node_modules"),
     path.resolve(packageRoot, "src"),
-    path.resolve(packagesRoot, "react-components-core"),
-    path.resolve(packagesRoot, "react-hooks"),
-    path.resolve(packagesRoot, "react-native-utils"),
-    path.resolve(packagesRoot, "react-utils"),
-    path.resolve(packagesRoot, "react-types"),
-    path.resolve(workspaceRoot, "packages/react-native-styled"),
 ];
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+// Seems like it is not necessary to enable this.
+// Causes the following errors:
+// - Property 'SharedArrayBuffer' doesn't exist error
+// - Cannot read property 'URLSearchParams' of undefined
+// - Cannot read property 'prototype' of undefined
+// config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
